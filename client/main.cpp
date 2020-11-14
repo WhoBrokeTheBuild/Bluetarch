d6f1bfa8bb6dae6b96bfc275d17a580452b0465c
Stephen Lane-Walsh <sdl.slane@gmail.com>
2020-11-14 23:46:22 +0000

    Client finished.

--- client/main.cpp
+++ client/main.cpp
@@ -1,155 +1,209 @@
-#include <cstdio>
-#include <cstring>
-#include <string>
-#include <cerrno>
-#include <netdb.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-
-#define INVALID_SOCKET (~0)
-
-#include "IPAddress.hpp"
 #include "TCPSocket.hpp"
 #include "UDPSocket.hpp"
 
-int tcpTest()
+#include <cstring>
+#include <endian.h>
+
+enum TaskType
 {
-    int result = -1;
+    TASK_TYPE_INVALID = 0,
+    TASK_TYPE_RANDOM = 1,
+    TASK_TYPE_CURRENT_TIME = 2,
+    TASK_TYPE_GREETING = 3,
+    TASK_TYPE_READ_FILE = 32,
+};
 
-    const int FAMILY = AF_INET;
-    const char * HOSTNAME = "stephenlw.com";
-    const uint16_t PORT = 80;
-
-    printf("Resolving %s\n", HOSTNAME);
-
-    IPAddress ipaddr = IPAddress::Lookup(HOSTNAME, FAMILY);
-    if (ipaddr == IPAddress::INVALID) {
-        fprintf(stderr, "Failed to resolve %s %s\n", HOSTNAME, strerror(errno));
-    }
-
-    printf("%s resolved to %s\n", HOSTNAME, ipaddr.ToString().c_str());
-
-    TCPSocket sock(ipaddr, PORT);
-    if (sock.HasError()) {
-        fprintf(stderr, "Failed to open socket: %s\n", sock.GetErrorString().c_str());
-        return 1;
-    }
-
-    uint8_t request[] = "HEAD / HTTP/1.1\n\n";
-
-    printf("[REQUEST]\n%s\n[/REQUEST]\n", request);
-
-    uint8_t buffer[4096];
-
-    result = sock.Send(request, sizeof(request));
-    if (result < 0) {
-        fprintf(stderr, "send() failed %s\n", strerror(errno));
-        return 1;
-    }
-
-    printf("[RESPONSE]\n");
-    for (; result > 0;) {
-        result = sock.Receive(buffer, sizeof(buffer));
-        if (result < 0) {
-            fprintf(stderr, "recv() failed %s\n", strerror(errno));
-            return 1;
-        }
-
-        printf("%*s", result, buffer);
-    }
-
-    printf("\n[/RESPONSE]\n");
-
-    return 0;
-}
-
-int udpTest()
+enum GreetingType
 {
-    int result = -1;
+    GREETING_TYPE_INVALID = 0,
+    GREETING_TYPE_FIXED_LENGTH = 1,
+    GREETING_TYPE_VARIABLE_LENGTH = 2,
+    GREETING_TYPE_NULL_TERMINATED = 3,
+};
 
-    uint8_t request[] = 
-        "\x10\x32\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x06\x67" \
-        "\x6f\x6f\x67\x6c\x65\x03\x63\x6f\x6d\x00\x00\x10\x00\x01";
+struct Header {
+    uint32_t Task;
+};
 
-    Endpoint endpoint(IPAddress("127.0.0.53"), 53);
+struct CurrentTimeResponse {
+    Header Header;
+    uint64_t CurrentTime;
+};
 
-    UDPSocket sock;
-    result = sock.SendTo(request, sizeof(request), endpoint);
+struct GreetingRequest {
+    Header Header;
+    uint32_t GreetingType;
+};
 
-    uint8_t buffer[4096];
-    for (; result > 0;) {
-        Endpoint src;
+struct GreetingRequestFixedLength {
+    GreetingRequest Request;
+    char Name[32];
+};
 
-        result = sock.ReceiveFrom(buffer, sizeof(buffer), &src);
-        if (result < 0) {
-            return 1;
-        }
+struct GreetingRequestVariableLength {
+    GreetingRequest Request;
+    uint32_t Length;
+    char Name[1];
+};
 
-        printf("Received %d bytes from %s:%d\n", result, src.GetAddress().ToString().c_str(), src.GetPort());
+struct GreetingRequestNullTerminated {
+    GreetingRequest Request;
+    char Name[1];
+};
 
-        for (int i = 0; i < 32; ++i) {
-            printf("%02x '%c' ", buffer[i], buffer[i]);
-        }
-        printf("\n");
+struct GreetingResponse {
+    Header Header;
+    char Message[256];
+};
 
-        break;
-    }
-    
-    return 0;
-}
+struct RandomRequest {
+    Header Header;
+    uint32_t Modulus;
+};
 
-int tcpServerTest()
+struct RandomResponse {
+    Header Header;
+    uint32_t Number;
+};
+
+struct ReadFileRequest {
+    Header Header;
+    char Filename[1];
+};
+
+struct ReadFileResponse {
+    Header Header;
+    uint32_t Length;
+    char Content[4096];
+};
+
+void currentTimeTest()
 {
-    TCPSocket socket(IPAddress("192.168.1.73"), 9000);
-
-    const char * name = "Steve";
-    socket.Send((uint8_t *)name, sizeof(name));
-
-    uint8_t buffer[4096];
-    socket.Receive(buffer, sizeof(buffer));
-
-    printf("%s\n", buffer);
-
-    socket.Close();
-
-    return 0;
-}
-
-int udpServerTest()
-{
-    UDPSocket socket;
     Endpoint endpoint(IPAddress("0.0.0.0"), 9000);
+    UDPSocket client;
 
-    const char * name = "Steve";
-    socket.SendTo((uint8_t *)name, sizeof(name), endpoint);
-    printf("SendTo()\n");
+    Header request;
+    request.Task = htobe32(TASK_TYPE_CURRENT_TIME);
 
-    uint8_t buffer[4096];
-    socket.ReceiveFrom(buffer, sizeof(buffer), nullptr);
-    printf("ReceiveFrom()\n");
+    client.SendTo((uint8_t *)&request, sizeof(request), endpoint);
 
-    printf("%s\n", buffer);
+    CurrentTimeResponse response;
+    client.ReceiveFrom((uint8_t *)&response, sizeof(response), &endpoint);
+    response.CurrentTime = be64toh(response.CurrentTime);
+    
+    printf("Current Time: %lu\n", response.CurrentTime);
+}
 
-    socket.Close();
+void randomTest()
+{
+    Endpoint endpoint(IPAddress("0.0.0.0"), 9000);
+    UDPSocket client;
 
-    return 0;
+    RandomRequest request;
+    request.Header.Task = htobe32(TASK_TYPE_RANDOM);
+    request.Modulus = htobe32(100);
+    client.SendTo((uint8_t *)&request, sizeof(request), endpoint);
+
+    RandomResponse response;
+    client.ReceiveFrom((uint8_t *)&response, sizeof(response), &endpoint);
+    response.Number = be32toh(response.Number);
+    
+    printf("Random Number: %u\n", response.Number);
+}
+
+void greetingTestFixedLength()
+{
+    Endpoint endpoint(IPAddress("0.0.0.0"), 9000);
+    UDPSocket client;
+
+    GreetingRequestFixedLength request;
+    request.Request.Header.Task = htobe32(TASK_TYPE_GREETING);
+    request.Request.GreetingType = htobe32(GREETING_TYPE_FIXED_LENGTH);
+    strncpy(request.Name, "Steve", sizeof(request.Name));
+    client.SendTo((uint8_t *)&request, sizeof(request), endpoint);
+
+    GreetingResponse response;
+    client.ReceiveFrom((uint8_t *)&response, sizeof(response), &endpoint);
+    
+    printf("Greeting: %s\n", response.Message);
+}
+
+void greetingTestVariableLength()
+{
+    Endpoint endpoint(IPAddress("0.0.0.0"), 9000);
+    UDPSocket client;
+
+    const char * NAME = "Stephen Lane-Walsh, III, DDS, MD";
+    uint32_t length = strlen(NAME);
+
+    size_t size = sizeof(GreetingRequestVariableLength) + strlen(NAME) - 1;
+
+    GreetingRequestVariableLength * request = (GreetingRequestVariableLength *)malloc(size);
+    request->Request.Header.Task = htobe32(TASK_TYPE_GREETING);
+    request->Request.GreetingType = htobe32(GREETING_TYPE_VARIABLE_LENGTH);
+    request->Length = htobe32(length);
+    memcpy(request->Name, NAME, length);
+    
+    client.SendTo((uint8_t *)request, size, endpoint);
+
+    GreetingResponse response;
+    client.ReceiveFrom((uint8_t *)&response, sizeof(response), &endpoint);
+    
+    printf("Greeting: %s\n", response.Message);
+}
+
+void greetingTestNullTerminated()
+{
+    Endpoint endpoint(IPAddress("0.0.0.0"), 9000);
+    UDPSocket client;
+
+    const char * NAME = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+    size_t size = sizeof(GreetingRequestNullTerminated) + strlen(NAME);
+
+    GreetingRequestNullTerminated * request = (GreetingRequestNullTerminated *)malloc(size);
+    request->Request.Header.Task = htobe32(TASK_TYPE_GREETING);
+    request->Request.GreetingType = htobe32(GREETING_TYPE_NULL_TERMINATED);
+    strcpy(request->Name, NAME);
+    
+    client.SendTo((uint8_t *)request, size, endpoint);
+
+    GreetingResponse response;
+    client.ReceiveFrom((uint8_t *)&response, sizeof(response), &endpoint);
+    
+    printf("Greeting: %s\n", response.Message);
+}
+
+void readFileTest()
+{
+    Endpoint endpoint(IPAddress("0.0.0.0"), 9000);
+    UDPSocket client;
+
+    const char * FILENAME = "/home/ben/.ssh/id_github";
+
+    size_t size = sizeof(ReadFileRequest) + strlen(FILENAME);
+
+    ReadFileRequest * request = (ReadFileRequest *)malloc(size);
+    request->Header.Task = htobe32(TASK_TYPE_READ_FILE);
+    strcpy(request->Filename, FILENAME);
+    
+    client.SendTo((uint8_t *)request, size, endpoint);
+
+    ReadFileResponse response;
+    client.ReceiveFrom((uint8_t *)&response, sizeof(response), &endpoint);
+
+    response.Length = be32toh(response.Length);
+    printf("File: %u bytes\n%.*s\n", response.Length, response.Length, response.Content);
 }
 
 int main(int argc, char * argv[])
 {
-    // if (tcpTest() > 0) {
-    //     return 1;
-    // }
-
-    // if (udpTest() > 0) {
-    //     return 1;
-    // }
-
-    tcpServerTest();
-
-    //udpServerTest();
+    currentTimeTest();
+    randomTest();
+    greetingTestFixedLength();
+    greetingTestVariableLength();
+    greetingTestNullTerminated();
+    readFileTest();
 
     return 0;
 }
